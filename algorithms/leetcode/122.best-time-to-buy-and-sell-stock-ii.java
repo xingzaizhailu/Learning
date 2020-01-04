0b8d5e19581bf5f4093eeed0164f8a1c9f1fa209
Leo Wu <wutexuanleo@outlook.com>
2020-01-04 23:58:08 +1100

    122.best-time-to-buy-and-sell-stock-ii.java

---
+++ algorithms/leetcode/122.best-time-to-buy-and-sell-stock-ii.java
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode id=122 lang=java
+ *
+ * [122] Best Time to Buy and Sell Stock II
+ */
+
+// @lc code=start
+class Solution {
+    public int maxProfit(int[] prices) {
+        int profit = 0;
+        for (int i = 1; i < prices.length; ++i) {
+            int diff = prices[i] - prices[i-1];
+            if (diff > 0) profit += diff;
+        }
+        return profit;
+    }
+}
+// @lc code=end
+
