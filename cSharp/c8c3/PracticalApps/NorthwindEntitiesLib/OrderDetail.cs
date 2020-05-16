90fc1ab8d26b29272564dd8a93ed13b3c2766a27
Leo Wu <wutexuanleo@outlook.com>
2020-05-16 10:10:56 +1000

    add: chap14 - NorthwindContextLib and NorthwindEntitiesLib

---
+++ cSharp/c8c3/PracticalApps/NorthwindEntitiesLib/OrderDetail.cs
@@ -0,0 +1,14 @@
+﻿namespace Packt.Shared
+{
+    public class OrderDetail
+    {
+        public int OrderID { get; set; }
+        public int ProductID { get; set; }
+        public decimal UnitPrice { get; set; } = 0;
+        public short Quantity { get; set; } = 1;
+        public double Discount { get; set; } = 0;
+
+        public Order Order { get; set; }
+        public Product Product { get; set; }
+    }
+}
