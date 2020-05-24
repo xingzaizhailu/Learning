3bda6d566ce921d96a3c801a9e4eba2c2553e1f5
Leo Wu <wutexuanleo@outlook.com>
2020-05-24 21:33:28 +1000

    static and default files

---
+++ cSharp/c8c3/PracticalApps/NorthwindWeb/Startup.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace NorthwindWeb
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // run
+                // $ dotnet dev-certs https --trust
+                // to enable https
+                app.UseHsts();
+                // If a website specifies hsts and a browser supports it
+                // it forces all comm over HTTPS
+            }
+
+            app.UseRouting();
+
+            app.UseHttpsRedirection();  // redirect HTTP requests to HTTPS
+
+            app.UseDefaultFiles();      // index.html, default.html and so on
+            app.UseStaticFiles();
+
+            app.UseEndpoints(endpoints =>
+            {
+                // endpoints.MapGet("/", async context =>
+                // {
+                //     await context.Response.WriteAsync("Hello World!");
+                // });
+            });
+        }
+    }
+}
